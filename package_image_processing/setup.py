7b334a9359c03201dfb9ac8b643a29d952f7799f
Tiago Pimenta <104909118+Tipimenta@users.noreply.github.com>
2022-11-09 15:13:38 -0300

    Update setup.py
    Add url

--- package_image_processing/setup.py
+++ package_image_processing/setup.py
@@ -14,7 +14,7 @@ setup(
     description="Test version Image processing package using skimage. This project belongs to Karina Tiemi Kato.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Tipimenta/Bootcamp-Unimed-BH-Ciencia-de-Dados-DiO/tree/master/package_image_processing",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.5',
